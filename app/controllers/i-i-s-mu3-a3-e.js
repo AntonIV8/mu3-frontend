bcd84eeb0d887d5d4b6af85cff6629d9262d2e86
AntonIV8 <emberplugin@mail.ru>
2020-04-07 07:12:42 +0000

    Flexberry code generation

--- app/controllers/i-i-s-mu3-a3-e.js
+++
@@ -1,5 +0,0 @@
-import EditFormController from 'ember-flexberry/controllers/edit-form';
-
-export default EditFormController.extend({
-  parentRoute: 'i-i-s-mu3-a3-l',
-});
--- app/controllers/i-i-s-mu3-a3-e/new.js
+++
@@ -1,4 +0,0 @@
-import IISmu3A3EController from '../i-i-s-mu3-a3-e';
-
-var IISmu3A3ENewController = IISmu3A3EController;
-export default IISmu3A3ENewController;
