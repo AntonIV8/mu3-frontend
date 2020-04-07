5f42f12d8b7c4946fc4e11116cc02950e93f36a8
AntonIV8 <emberplugin@mail.ru>
2020-04-07 07:10:08 +0000

    Flexberry code generation

---
+++ app/controllers/i-i-s-mu3-a3-e/new.js
@@ -0,0 +1,4 @@
+import IISmu3A3EController from '../i-i-s-mu3-a3-e';
+
+var IISmu3A3ENewController = IISmu3A3EController;
+export default IISmu3A3ENewController;
---
+++ app/locales/en/forms/i-i-s-mu3-a3-e.js
@@ -0,0 +1,3 @@
+export default {
+  caption: 'IISmu3A3E'
+};
